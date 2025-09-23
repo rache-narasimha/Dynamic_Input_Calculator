87c9151b415a4eb09b71cd15825c9140f5252aaa
Narasimha Rache <narasimharache@MacBook-Air.local>
2025-09-23 18:30:25 +0530

    latest changes

--- src/com/dynamic_IP_calculator/logic/CoreLogic.java
+++ src/com/dynamic_IP_calculator/logic/CoreLogic.java
@@ -1,60 +1,55 @@
 package com.dynamic_IP_calculator.logic;
-import com.dynamic_IP_calculator.main.*;
-import java.util.*;
 
 public class CoreLogic {
-	//static Scanner sc = new Scanner (System.in);
-	
-	//static //double num1;
-	//double num2;
-	//double Result;
-	//String action ;	
 
-	public static void simpleCalcApp(double num1 , double num2 , String action){
+	public static void simpleCalcApp(double num1, double num2, int action) {
+
+		double Result;
+
+		//switch case
 		
-		/*double add =num1+num2;
-		double sub= num1-num2;
-		double mult=num1*num2;
-		double div = num1/num2;
-		double mod = num1%num2;*/
+		switch (action) {
+		case 1:
+			Result = num1 + num2;
+			System.out.println("your result is " + Result);
+			break;
+		case 2:
+			Result = num1 - num2;
+			System.out.println("your result is " + Result);
+			break;
+		case 3:
+			Result = num1 * num2;
+			System.out.println("your result is " + Result);
+			break;
+		case 4:
+			Result = num1 / num2;
+			System.out.println("your result is " + Result);
+			break;
+		case 5:
+			Result = num1 % num2;
+			System.out.println("your result is " + Result);
+			break;
+			default :
+				System.out.println("you have selected invalid option");
+
+		}
 		
-		
-		// CoreLogic action =new CoreLogic();
-		 
-			
-		
-		    
-		//	action = sc.next();
-		//String action ="add";
-		 
-		double  Result ;
-		 
-		 if(action=="add") {
-			 Result = num1 +num2;	
-			 System.out.println("your result is "+Result);
-			 
-		 }
-		 else if(action=="sub"){
-			 Result = num1- num2;
-			 System.out.println("your result is "+ Result);
-		 }
-		 else if(action=="mult"){
-			 Result = num1*num2;
-			 System.out.println("your result is "+ Result);
-		 }
-		 else if(action=="div"){
-			 Result = num1/num2;
-			 System.out.println("your result is "+ Result);
-		 }
-		 else if(action=="mod"){
-			 Result = num1%num2;
-			 System.out.println("your result is "+ Result);
-		 }
-		 else {
-			 System.out.println("invalid input, please try with available options");
-		 }
-	
-		
-		
+		// else if ladder
+
+		/*
+		 * if(action.equalsIgnoreCase("add")) { Result = num1 +num2;
+		 * System.out.println("your result is "+Result);
+		 * 
+		 * } else if(action.equalsIgnoreCase("sub")){ Result = num1- num2;
+		 * System.out.println("your result is "+ Result); } else
+		 * if(action.equalsIgnoreCase("mult")){ Result = num1*num2;
+		 * System.out.println("your result is "+ Result); } else
+		 * if(action.equalsIgnoreCase("div")){ Result = num1/num2;
+		 * System.out.println("your result is "+ Result); } else
+		 * if(action.equalsIgnoreCase("mod")){ Result = num1%num2;
+		 * System.out.println("your result is "+ Result); } else {
+		 * System.out.println("invalid input, please try with available options"); }
+		 */
+
 	}
 }
--- src/com/dynamic_IP_calculator/main/CalcLogics_Main.java
+++ src/com/dynamic_IP_calculator/main/CalcLogics_Main.java
@@ -2,59 +2,47 @@ package com.dynamic_IP_calculator.main;
 
 import java.util.Scanner;
 import com.dynamic_IP_calculator.logic.*;
-import com.dynamic_IP_calculator.logic.*;
 
 public class CalcLogics_Main {
-static Scanner sc = new Scanner(System.in);
-
+//static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome");
 		System.out.println("you can perform following actions-");
-		System.out.println("1)add");
-		System.out.println("2)sub");
-		System.out.println("3)mult");
-		System.out.println("4)div");
-		System.out.println("5)mod");
+		System.out.println("for addition, please type '1'");
+		System.out.println("for substraction, please type '2'");
+		System.out.println("for multiplication, please type '3'");
+		System.out.println("for divison, please type '4'");
+		System.out.println("for modulus, please type'5'");
 		System.out.println("please type the action you want to perform");
- 
-		String action ;
-			
-			
-		    
-			action = sc.next();
-	   	    
+
+		int action;
+
+		Scanner sc = new Scanner(System.in);
+
+		action = sc.nextInt();
+if (action>5) {
+	System.out.println("you have selected invalid option, please select with in the available options");
+}
+else if (action<=5 && action >=0){
+	
+
 		double num1;
 		double num2;
 
-	//	num1 =sc.nextDouble();
-	//	num2 =sc.nextDouble();
-        
-		
-		
-				
 		System.out.println("Enter your 1st Value ");
-		 num1 =sc.nextDouble();
-		 
-		 
-		 System.out.println("Enter your 2nd value");
-		 num2 =sc.nextDouble();
-		 
-		 
-		
-		 CoreLogic.simpleCalcApp( num1 ,  num2 , action);
-		
-		
-		
-		
-	
-		
-		/*int sum = num1+num2;
-		System.out.println("your sum is = "+sum);*/
-		
-		
+		num1 = sc.nextDouble();
+
+		System.out.println("Enter your 2nd value");
+		num2 = sc.nextDouble();
+
+		sc.close();
+
+		CoreLogic.simpleCalcApp(num1, num2, action);
 
 	}
 
 }
+	}
+	
